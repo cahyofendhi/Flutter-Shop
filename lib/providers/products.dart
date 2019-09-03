a96dde4a6a4392caf602aa6ad5a6d2922aaa6709
Bekti Cahyo <Bektic.22@gmail.com>
2019-09-03 13:29:06 +0700

    custome error handling

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:shop/models/http_exception.dart';
 
 import 'product.dart';
 
@@ -132,8 +133,18 @@ class Products with ChangeNotifier {
     } else {}
   }
 
-  void deleteProduct(String id) {
-    _items.removeWhere((prod) => prod.id == id);
+  Future<void> deleteProduct(String id) async {
+    final url = 'https://flutter-shop-f97f7.firebaseio.com/product/$id.json';
+    final existingProductIndex = _items.indexWhere((prod) => prod.id == id);
+    var existingProduct = _items[existingProductIndex];
+    _items.removeAt(existingProductIndex);
     notifyListeners();
+    final response = await http.delete(url);
+    if (response.statusCode >= 400) {
+      _items.insert(existingProductIndex, existingProduct);
+      notifyListeners();
+      throw HttpException('Could not delete product');
+    }
+    existingProduct = null;
   }
 }
